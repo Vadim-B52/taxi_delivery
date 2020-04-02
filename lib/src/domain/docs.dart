7808a638b6169bbe87c51f8e3376d28e4ca3e024
Vadim Belotitskiy <vadim-b@yandex-team.ru>
2020-04-02 17:28:50 +0300

    add docs

--- lib/src/domain/docs.dart
+++ lib/src/domain/docs.dart
@@ -1,5 +1,7 @@
 /*
 
+https://www.figma.com/file/OdsOGd8vWQhMxjaoTjtIvM/%D0%91%D0%B5%D1%80%D1%83-%D0%B4%D0%BE%D1%81%D1%82%D0%B0%D0%B2%D0%BA%D0%B0?node-id=0%3A1
+
 https://www.figma.com/file/OdsOGd8vWQhMxjaoTjtIvM/Беру-доставка?node-id=0%3A1
 
 
